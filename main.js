1a92a5edf31816649b14b36d6b0f56e503b6ab45
Daasshaas <30hanenko.dasha@gmail.com>
2024-12-17 14:13:44 +0200

    new

---
+++ main.js
@@ -0,0 +1,36 @@
+let age = "15"; 
+let name = "Dasha";
+let isStudent = "true";
+let myString = "When you're having a hard time, always remind yourself that if you give up, it won't get better";
+let myNumber = 10;
+let myNull = null; 
+
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(myString)
+console.log(myNumber + 10);
+console.log(myNull); 
+let userName = prompt("Як вас звати?");
+console.log(userName);
+
+let result = confirm("Підтвердити дію на сайті");
+console.log(result);
+if (Boolean (true)) {
+    alert("Дякую за підтвердження!");
+}
+
+if (Boolean (false)) {
+   alert("Дію відмінено!");
+}
+
+alert("Дія небезпечна");
+let result2 = confirm("Підтвердити дію на сайті");
+console.log(result2);
+if (Boolean (true)) {
+    alert("Дякую за підтвердження!");
+}
+
+if (Boolean (false)) {
+   alert("Дію відмінено!");
+}
